1839798bbf05f321a80e4c878ab240f55d604943
zekeriyaince <38784907+zekeriyaince@users.noreply.github.com>
2018-04-27 14:25:20 +0300

    C dilinde alınan sayıyı ekrana basan program
    development by Zekeriya İnce

---
+++ EkranaSayiBasDeneme.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+  int alinanSayi;
+  printf("Gosterilmek istenen sayiyi giriniz:\n");
+  scanf("%d",&alinanSayi);
+  printf("Alinan sayi : %d",alinanSayi);
+  getch(); //console ekrani bekletiliyor.
+  return 0; 
+}
